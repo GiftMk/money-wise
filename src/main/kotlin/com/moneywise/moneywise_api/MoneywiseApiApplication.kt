2a96b0f2b9a667afccb2455a429f7d415eb04f32
Gift Mkwara <gift.mkwara@gmail.com>
2024-06-22 19:16:37 +1200

    initial commit

---
+++ src/main/kotlin/com/moneywise/moneywise_api/MoneywiseApiApplication.kt
@@ -0,0 +1,11 @@
+package com.moneywise.moneywise_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoneywiseApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoneywiseApiApplication>(*args)
+}
